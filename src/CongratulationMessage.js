3779667bb80bd42b3d73b637b7289d402d666875
Istvan Schulcz <schulczistvanjr@gmail.com>
2021-12-22 20:23:42 +0100

    DisplayIf (children prop)

--- src/CongratulationMessage.js
+++ src/CongratulationMessage.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 
-export const CongratulationMessage = ({ numberOfClicks, threshold, onHide }) => {
+export const CongratulationMessage = ({ threshold, onHide }) => {
     useEffect(() => {
         return () => console.log("Congrats message onmounted");
     }, []);
 
-    return numberOfClicks >= threshold
-        ? <>
+    return (
+        <>
             <h1>Congratulation! You've reached {threshold} number of clicks!</h1>
             <button onClick={onHide}>Hide</button>
         </>
-        : null
+    );
 }
--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -3,11 +3,12 @@ import { CounterButton } from "../CounterButton";
 import { CongratulationMessage } from "../CongratulationMessage";
 import { useLocation } from "react-router-dom";
 import { parse } from "query-string";
+import { DisplayIf } from "../DisplayIf";
 
 export const CounterButtonPage = () => {
     const location = useLocation();
-    // const startingValue = parse(location.search).startingValue;
-    const [numberOfClicks, setNumberOfClicks] = useState(0); //useState(Number(startingValue));
+    const startingValue = parse(location.search).startingValue || 0;
+    const [numberOfClicks, setNumberOfClicks] = useState(Number(startingValue));
     const [hideMessage, setHideMessage] = useState(false);
 
     const increment = () => { setNumberOfClicks(numberOfClicks + 1); }
@@ -15,12 +16,11 @@ export const CounterButtonPage = () => {
     return (
         <>
         <h1>Counter Button Page</h1>
-        {hideMessage
-        ? null
-        : <CongratulationMessage
-            numberOfClicks={numberOfClicks}
-            threshold={10}
-            onHide={() => setHideMessage(true)} />}
+        <DisplayIf condition={!hideMessage && numberOfClicks >= 10}>
+            <CongratulationMessage
+                threshold={10}
+                onHide={() => setHideMessage(true)} />    
+        </DisplayIf>
         <CounterButton
             onIncrement={increment}
             numberOfClicks={numberOfClicks} />
